03454c7dc06c61b736cfde0f4cb6b5600d77834d
Niklas von Heyden <niklas.heyden@me.com>
2021-03-18 16:49:33 +0100

    connect state to build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import classes from './BuildControls.module.css'
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Cheese', type: 'cheese'},
+  { label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => (
-  <div>
-
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        type={ctrl.type}
+        added={() => props.ingredientAdded(ctrl.type)} />
+    ))}
   </div>
 );
 
