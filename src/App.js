9727bd86aee08b380d43fa83ff35426adfdca982
Niklas von Heyden <niklas.heyden@me.com>
2021-03-28 20:32:17 +0200

    implement navigation links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Order/Orders';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
